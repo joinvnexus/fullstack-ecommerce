4d30984d75daec0bfcec59f116b6fe8658e92267
Joinvnexus <projoynaidu407@gmail.com>
2026-01-22 14:57:32 +0600

    Remove sensitive and build files from Git: backend/.env and backend/dist/

--- backend/dist/controllers/admin/productController.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-export declare const getProducts: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const createProduct: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const updateProduct: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const deleteProduct: (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const bulkUpdateProducts: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=productController.d.ts.map
